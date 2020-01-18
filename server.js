a1106818a95b042a1d1bf8b93994c8f67f3b5306
timlo <a0987642936@yahoo.com>
2020-01-18 09:40:38 +0800

    Divide the database connection: Local and Cloud

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 // Server should be starting file
 const app = require('./app');
 
+dotenv.config({ path: './config.env' })
 
-// console.log(app.get('env'))
-// console.log(process.env)
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology:true
+}).then(con =>{
+    // console.log(con.connections)
+    console.log('DB connection successful')
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
