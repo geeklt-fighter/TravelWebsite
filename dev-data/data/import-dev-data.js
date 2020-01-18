53fdf3838577e86b52da37984a8846a90fa97928
timlo <a0987642936@yahoo.com>
2020-01-18 13:52:32 +0800

    Create the script used to load the sample data into mongodb

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+/** This script runs independeently from the express application */
+const fs = require('fs')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const Tour = require('../../models/tourModel')
+
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(con => {
+    // console.log(con.connections)
+    console.log('DB connection successful')
+})
+
+
+// Read the json file 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`,'utf-8'))
+
+// import data into database
+const importData = async()=>{
+    try {
+        await Tour.create(tours)    // Note: please use the object rather than string
+        console.log('Data successfully loaded !')
+        process.exit()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+// Delete all the data rom collections
+const deleteData = async()=>{
+    try {
+        await Tour.deleteMany()
+        console.log('Data successfully deleted !')
+        process.exit()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+if (process.argv[2]=== '--import') {
+    importData()
+}else if(process.argv[2]=== '--delete'){
+    deleteData()
+}
+// console.log(process.argv)
