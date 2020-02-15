619c3a2821f00819809665f3a815d53525d3660e
timlo <a0987642936@yahoo.com>
2020-02-15 12:00:26 +0800

    Implement upload image to change the user portfolio

--- public/js/index.js
+++ public/js/index.js
@@ -34,10 +34,13 @@ if (logoutButton) {
 if (userDataForm) {
     userDataForm.addEventListener('submit', e => {
         e.preventDefault()
-        const name = document.getElementById('name').value
-        const email = document.getElementById('email').value
+        const form = new FormData()
+        form.append('name', document.getElementById('name').value)
+        form.append('email', document.getElementById('email').value)
+        form.append('photo', document.getElementById('photo').files[0])
 
-        updateSettings({ name, email }, 'data')
+        console.log(form)
+        updateSettings(form, 'data')
     })
 }
 
@@ -45,14 +48,14 @@ if (userDataForm) {
 if (userPasswordForm) {
     userPasswordForm.addEventListener('submit', async e => {
         e.preventDefault()
-        document.querySelector('.btn--save-password').textContent='Updating ...' 
+        document.querySelector('.btn--save-password').textContent = 'Updating ...'
         const passwordCurrent = document.getElementById('password-current').value
         const password = document.getElementById('password').value
         const passwordConfirm = document.getElementById('password-confirm').value
         await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password')
 
 
-        document.querySelector('.btn--save-password').textContent='Save password' 
+        document.querySelector('.btn--save-password').textContent = 'Save password'
         document.getElementById('password-current').value = ''
         document.getElementById('password').value = ''
         document.getElementById('password-confirm').value = ''
