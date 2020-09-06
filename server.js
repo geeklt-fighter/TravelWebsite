cd4873bdf2b95751b46951c8e48c46344fd0c632
timlo <a0987642936@yahoo.com>
2020-09-06 11:05:35 +0800

    Follow eslint rule to delete console.log

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 process.on('uncaughtException', err => {
-    console.log(err.name, err.message)
-    console.log('UNCAUGHT EXCETION! Shutting down ...')
+    // console.log(err.name, err.message)
+    // console.log('UNCAUGHT EXCETION! Shutting down ...')
     process.exit(1)
 })
 
@@ -20,20 +20,19 @@ mongoose.connect(DB, {
     useFindAndModify: false,
     useUnifiedTopology: true
 }).then(con => {
-    // console.log(con.connections)
-    console.log('DB connection successful')
+    // console.log('DB connection successful')
 })
 
 
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+    // console.log(`App running on port ${port}`);
 })
 
 
 process.on('unhandleRejection', err => {
-    console.log(err.name, err.message)
-    console.log('UNHANDLED REJECTION! Shutting down ...')
+    // console.log(err.name, err.message)
+    // console.log('UNHANDLED REJECTION! Shutting down ...')
     server.close(() => {
         process.exit(1)
     })
